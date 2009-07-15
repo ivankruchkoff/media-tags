7071aff61864c9016596fecea4e916b72210aca4
pmenard <pmenard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-07-15 16:46:33 +0000

    Major updates to the 2.0 version of plugin. See the readme.txt for details.
    git-svn-id: https://plugins.svn.wordpress.org/media-tags@135459 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/mediatags_config.php
@@ -0,0 +1,8 @@
+<?php
+define('MEDIA_TAGS_TAXONOMY', 'media-tags');
+define('MEDIA_TAGS_REWRITERULES','1');
+define('MEDIA_TAGS_QUERYVAR', 'media-tag');
+define('MEDIA_TAGS_URL', 'media-tags');
+define('MEDIA_TAGS_TEMPLATE', 'mediatag.php');
+
+?>
---
+++ trunk/mediatags_shortcodes.php
@@ -0,0 +1,48 @@
+<?php
+// [media-tags media_tags="alt-views,page-full,thumb" tags_compare="AND" orderby="menu_order" display_item_callback=""]
+
+function mediatags_shortcode_handler($atts, $content=null, $tableid=null) 
+{
+	global $post;
+	
+	if ((!isset($atts['return_type'])) || ($atts['return_type'] != "li"))
+		$atts['return_type'] = "li";
+
+	if ((!isset($atts['display_item_callback'])) || (strlen($atts['display_item_callback']) == 0))
+		$atts['display_item_callback'] = 'default_item_callback';
+
+	if ((isset($atts['post_parent'])) && ($atts['post_parent'] == "this"))
+		$atts['post_parent'] = $post->ID;
+		
+	$atts['call_source'] = "shortcode";
+		
+	//echo "atts<pre>"; print_r($atts); echo "</pre>";
+	
+	if (!is_object($mediatags)) 
+		$mediatags = new MediaTags();
+		
+	$output = $mediatags->get_attachments_by_media_tags($atts);
+	if ($output)
+	{
+		if (isset($atts['before_list']))
+		{
+			$output = $atts['before_list'] . $output;
+		}
+		
+		if (isset($atts['after_list']))
+		{
+			$output = $output .$atts['after_list'];			
+		}
+	}
+	return $output;
+}
+
+function default_item_callback($post_item)
+{
+	//echo "post_item<pre>"; print_r($post_item); echo "</pre>";
+	
+	return '<li class="media-tag-list" id="media-tag-item-'.$post_item->ID.'"><img src="'.
+	 	wp_get_attachment_url($post_item->ID).'" 
+			title="'.$post_item->post_title.'" /></li>';
+}
+?>
