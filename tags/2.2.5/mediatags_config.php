1030a78e24508130bffc2cf32eb8d501931d3099
pmenard <pmenard@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-30 17:20:18 +0000

    tagging version 2.2.5
    git-svn-id: https://plugins.svn.wordpress.org/media-tags@150563 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/2.2.5/mediatags_config.php
@@ -0,0 +1,21 @@
+<?php
+define('MEDIA_TAGS_VERSION', "2.0");
+define('MEDIA_TAGS_DATA_VERSION', "2.0");
+
+define('MEDIA_TAGS_TAXONOMY', 'media-tags');
+
+define('ADMIN_MENU_KEY', 'media-tags');
+define('MEDIA_TAGS_REWRITERULES','1');
+
+define('MEDIA_TAGS_URL_DEFAULT', MEDIA_TAGS_TAXONOMY);
+$mediatag_base = get_option('mediatag_base');
+// Need to come up with validation logic here.
+if (!$mediatag_base)
+	$mediatag_base = "media-tags";
+define('MEDIA_TAGS_URL', $mediatag_base);
+
+define('MEDIA_TAGS_QUERYVAR', 'media-tag');
+
+define('MEDIA_TAGS_TEMPLATE', 'mediatag.php');
+
+?>
