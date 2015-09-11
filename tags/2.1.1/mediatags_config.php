b68bb73441e497482d89c536223548770ccb4a4c
Ivan Kruchkoff <ivank@10up.com>
2015-09-11 11:52:37 -0400

    Moving all files to root of repo

--- tags/2.1.1/mediatags_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('MEDIA_TAGS_TAXONOMY', 'media-tags');
-define('MEDIA_TAGS_REWRITERULES','1');
-define('MEDIA_TAGS_QUERYVAR', 'media-tag');
-define('MEDIA_TAGS_URL', 'media-tags');
-define('MEDIA_TAGS_TEMPLATE', 'mediatag.php');
-
-?>
